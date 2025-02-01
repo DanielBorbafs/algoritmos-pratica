47416ee60e58172b7e329ef37c636030f6d22e14
Danielborbafs <dev.danielborba@gmail.com>
2025-02-01 05:46:21 -0300

    feat: function para alternar o display das perguntas

--- positivus/js/script.js
+++ positivus/js/script.js
@@ -4,3 +4,22 @@ const navbar = document.getElementsByClassName('navbar_list')[0];
 btnResponsivo.addEventListener('click', function () {
   navbar.classList.toggle('active');
 });
+
+/* ============ Trocando classe =============== */
+
+// Garantir que o script seja executado depois que o DOM for carregado
+document.addEventListener('DOMContentLoaded', function () {
+  const arrowQuestion = document.getElementById('arrow');
+  const textOpen = document.getElementById('texto_pergunta');
+
+  // Adiciona o evento de clique
+  arrowQuestion.addEventListener('click', function () {
+    console.log('Elemento clicado');
+    // Alterna a visibilidade do texto
+    if (textOpen.style.display === 'none' || textOpen.style.display === '') {
+      textOpen.style.display = 'block'; // Mostra o texto
+    } else {
+      textOpen.style.display = 'none'; // Esconde o texto
+    }
+  });
+});
